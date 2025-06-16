29982f4d26e941967bb8b0104c0ac7e31aca736b
gr2nade <gr2nade@gmail.com>
2025-06-16 17:37:44 +0900

    20250616 Set frmMain

---
+++ WFSolution/WindowsFormsApp/frmSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class frmSettings : Form
+    {
+        public frmSettings()
+        {
+            InitializeComponent();
+            this.Text = "Settings";
+            this.Size = new System.Drawing.Size(400, 300);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            Label lblMessage = new Label
+            {
+                Text = "Setting Screen",
+                Location = new System.Drawing.Point(150, 130),
+                AutoSize = true
+            };
+
+            this.Controls.Add(lblMessage);
+        }
+    }
+}
