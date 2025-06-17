7a70589e37414fe58a014e53bc66e75a3c353d08
gr2nade <gr2nade@gmail.com>
2025-06-17 09:19:56 +0900

    Set user32

---
+++ WFSolution/WindowsFormsApp/Interop/User32.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp.Interop
+{
+    public static class User32
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool SetForegroundWindow(IntPtr hwWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    }
+}
