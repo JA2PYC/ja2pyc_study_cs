b244fee67549f0e6214ae7266ca699087d141b19
gr2nade <gr2nade@gmail.com>
2025-06-18 14:12:41 +0900

    20250618 Set mv InSight

--- WFSolution/WindowsFormsApp/MachineVision/CameraManager.cs
+++ WFSolution/WindowsFormsApp/MachineVision/CameraManager.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Cognex VisionPro is a library for machine vision applications.
+using Cognex.VisionPro;
+using Cognex.VisionPro.Exceptions;
+
+
+
 namespace WindowsFormsApp.MachineVision
 {
     internal class CameraManager
     {
+        
     }
 }
---
+++ WFSolution/WindowsFormsApp/MachineVision/InSight/InSightManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cognex.InSight;
+using Cognex.InSight.EasyBuilder;
+
+namespace WindowsFormsApp.MachineVision.InSight
+{
+    internal class InSightManager
+    {
+
+
+    }
+}
