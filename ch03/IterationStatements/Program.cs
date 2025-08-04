a66bd4ebf530bcc54fa7eb382b62d2e56b4ca691
gr2nade <gr2nade@gmail.com>
2025-08-04 23:20:47 +0900

    Iteration Statements

--- ch03/IterationStatements/Program.cs
+++ ch03/IterationStatements/Program.cs
@@ -3,13 +3,13 @@ Console.WriteLine("Iteration Statements");
 Console.WriteLine("----------------------");
 
 // For loop example
-for (int i = 0; i <10; i++)
+for (int i = 0; i < 10; i++)
 {
     Console.WriteLine($"Iteration {i + 1}");
 }
 
 // Nested for loop example
-for ( int i = 0; i < 3; i++)
+for (int i = 0; i < 3; i++)
 {
     for (int j = 0; j < 2; j++)
     {
@@ -22,12 +22,35 @@ for ( int i = 0; i < 3; i++)
 int[] arr = [1, 2, 3, 4, 5];
 foreach (int element in arr)
 {
-       Console.WriteLine($"Foreach Array element: {element}");
+    Console.WriteLine($"Foreach Array element: {element}");
 }
 
 // While loop example
+int sum = 0;
+int n = 1;
 
+while (n <= 100)
+{
+    if (n % 2 == 0)
+    {
+        sum += n;
+    }
 
+    n++;
+}
+Console.WriteLine($"Sum of even numbers from 1 to 100: {sum}");
+
+// Do-while loop example
+sum = 0;
+n = 1;
+do
+{
+    if (n % 2 == 0)
+    {
+        sum += n;
+    }
+} while (++n <= 100);
+Console.WriteLine($"Sum of even numbers from 1 to 100 using do-while: {sum}");
 
 Console.WriteLine("----------------------");
 Console.WriteLine("Press any key to exit...");
